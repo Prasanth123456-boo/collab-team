facb63068645c44f8047bb130c4b3988b855f56f
Prasanth AV <aprasanth448@gmail.com>
2024-05-22 09:50:31 +0000

    Added h2

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <h1>Hello This is new page  </h1>
+      <h2>Feature added</h2>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure sit vitae assumenda saepe ut! Illum reprehenderit delectus enim excepturi corporis repudiandae in harum ea. Repellendus alias provident maxime quod accusantium.</p>
     </>
   )
